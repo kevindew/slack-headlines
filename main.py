5f6c6441436ec63ea75c09d13752112dbe9bd2da
Kevin Dew <kev@redbullet.co.uk>
2015-06-24 11:26:18 +0100

    More stuff - need env vars

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import json
 import datetime
 
 app = Flask(__name__)
-slack = Slacker('xoxp-2175777328-2178938430-6767734195-dc1344')
+slack = Slacker('xoxp-2175777328-2178938430-6784950080-de51a4')
 
 # Note: We don't need to call run() since our application is embedded within
 # the App Engine WSGI application server.
@@ -16,22 +16,37 @@ slack = Slacker('xoxp-2175777328-2178938430-6767734195-dc1344')
 @app.route('/headline', methods=['POST'])
 def hello():
     """Send a headline."""
-    # if request.form['token'] != 'zSf7gubrdsb798Lrdi18AkfS':
-        # return 'Bad Request', 400
+    if request.form['token'] != 'zSf7gubrdsb798Lrdi18AkfS':
+        return "Sorry - token doesn't match. \n" + request.form['text'], 400
+    if request.form['channel_name'] == 'directmessage': 
+        return "Headline in a direct message? what are you doing son.\n" + request.form['text'], 400
     icon_url = 'https://pbs.twimg.com/profile_images/1484608334/mcdonaldtre_400x400.jpg'
     username = 'Trevor McDonald'
     user = slack.users.info(request.form['user_id'])
+    fallback = '@' + request.form['user_name'] + ' just dropped the headline of ' + request.form['text']
     attachments = [{
+        'fallback': fallback,
+        'color': '#C9A150',
         'author_name': '@' + request.form['user_name'],
         'author_icon': user.body['user']['profile']['image_24'],
         'text': request.form['text'],
-        'fields': [{
-            'title': 'Discuss in',
+        # 'fields': [{
+        #     'title': 'Posted in',
+        #     'value': '#' + request.form['channel_name'],
+        #     'short': True
+        # }]
+    }]
+    parse = 'full'
+    slack.chat.post_message('#' + request.form['channel_name'], '', username=username, icon_url=icon_url, attachments=json.dumps(attachments), parse='full')
+
+    if request.form['channel_name'] != 'headlines': 
+        fallback = fallback + ' posted in #' + request.form['channel_name']
+        attachments[0]['fields'] = [{
+            'title': 'Posted in',
             'value': '#' + request.form['channel_name'],
             'short': True
         }]
-    }]
-    slack.chat.post_message('#playtime', '', username=username, icon_url=icon_url, attachments=json.dumps(attachments), parse='full')
+        slack.chat.post_message('#headlines', '', username=username, icon_url=icon_url, attachments=json.dumps(attachments), parse='full')        
 
     return '', 204
 
@@ -43,4 +58,4 @@ def page_not_found(e):
 @app.errorhandler(500)
 def application_error(e):
     """Return a custom 500 error."""
-    return 'Sorry, unexpected error: {}'.format(e), 500
+    return 'Sorry, unexpected error: {}'.format(e) + "\n" + request.form['text'], 500
